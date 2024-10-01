1a2fc1d1d75e7a53be20c9ed7dd1663d53176f78
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-10-01 18:19:27 +0530

    Initialize hellow_cargo project with basic structure and code

---
+++ Day 0/hellow_cargo/target/release/deps/hellow_cargo-707a5b3959b836a0.d
@@ -0,0 +1,5 @@
+/home/ravi/Desktop/WebDev/Rust/Day 0/hellow_cargo/target/release/deps/hellow_cargo-707a5b3959b836a0: src/main.rs
+
+/home/ravi/Desktop/WebDev/Rust/Day 0/hellow_cargo/target/release/deps/hellow_cargo-707a5b3959b836a0.d: src/main.rs
+
+src/main.rs:
